21b6d552a2212f1304c08468a0738802ff87ac84
Echoooo <echoshihtw@gmail.com>
2024-06-09 18:02:22 +0800

    feat: StringOutputParser

--- src/app/api/trivia/route.ts
+++ src/app/api/trivia/route.ts
@@ -1,19 +1,20 @@
 import { ChatOpenAI } from '@langchain/openai';
 import { PromptTemplate } from '@langchain/core/prompts';
-import { LLMChain } from 'langchain/chains';
+import { StringOutputParser } from '@langchain/core/output_parsers';
+
 
 const model = new ChatOpenAI({
   openAIApiKey: process.env.OPENAI_API_KEY,
   temperature: 0.9,
 });
 
-const prompt = PromptTemplate.fromTemplate(`Ask me a trivia about psychedelic drugs.`);
-const chain = new LLMChain({
-  llm: model,
-  prompt,
-});
+const makeQuestion = async () => {
+  const prompt = PromptTemplate.fromTemplate(`Ask me a trivia about psychedelic drugs.`);
+  const chain = prompt.pipe(model).pipe(new StringOutputParser());
+  return await chain.invoke();
+};
 
 export async function GET() {
-  const gptResponse = await chain.invoke();
-  return Response.json({ question: gptResponse.text });
+  const question = await makeQuestion();
+  return Response.json({ question });
 }
